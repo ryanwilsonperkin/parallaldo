f0a67dfdbad18688b3f2ea6e055288f7ebdbe622
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-09-30 17:14:32 -0400

    Proper defguard naming. (PARALLALDO, no S)

--- filenames.h
+++ filenames.h
@@ -1,7 +1,7 @@
-#ifndef PARALLALDOS_FILENAMES_H
-#define PARALLALDOS_FILENAMES_H
+#ifndef PARALLALDO_FILENAMES_H
+#define PARALLALDO_FILENAMES_H
 
 int listFilenames(const char *dir, char ***filenames);
 void freeFilenames(int nFiles, char **filenames);
 
-#endif // PARALLALDOS_FILENAMES_H
+#endif // PARALLALDO_FILENAMES_H
