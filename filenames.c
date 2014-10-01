5a7c91d2933ab661370ba2703a9ab0ac2e42255d
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-10-01 00:00:05 -0400

    Formatting and docs for filenames.c.

--- filenames.c
+++ filenames.c
@@ -8,42 +8,60 @@
 
 #include "filenames.h"
 
+// Combine directory and file name, allocate space.
+char *get_relative_filename(const char *dir, const char *filename)
+{
+    int dir_length = strlen(dir);
+    int filename_length = strlen(filename);
+
+    char *relative_filename = malloc((dir_length + filename_length + 2) * sizeof(char));
+    relative_filename = strcpy(relative_filename, dir);
+    relative_filename = strcat(relative_filename, "/");
+    relative_filename = strcat(relative_filename, filename);
+    return relative_filename;
+}
+
 // Code adapted from GNU libc manual: https://www.gnu.org/software/libc/manual/html_node/Simple-Directory-Lister.html
 int listFilenames(const char *dir, char ***filenames)
 {
     DIR *dp;
     struct dirent *ep;
     int i, n_files;
-    int dir_length, filename_length;
 
+    // Try to open directory.
     dp = opendir(dir);
     if (dp == NULL) {
         fprintf(stderr, "error: Could not open directory %s.\n", dir);
         return 0;
     }
 
+    // Increment number of files for each DT_REG entity found.
     n_files = 0;
     while (ep = readdir(dp)) {
-        if (ep->d_type == DT_REG) n_files++;
+        if (ep->d_type == DT_REG) {
+            n_files++;
+        }
     }
+
+    // Allocate a string pointer for each filename.
     *filenames = malloc(n_files * sizeof(char *));
+
+    // Return to first entity in directory listing.
     rewinddir(dp);
 
+    // Allocate filename for each DT_REG entity found.
     i = 0;
-    dir_length = strlen(dir);
     while (ep = readdir(dp)) {
         if (ep->d_type == DT_REG) {
-            filename_length = strlen(ep->d_name);
-            (*filenames)[i] = malloc((dir_length + filename_length + 2) * sizeof(char));
-            (*filenames)[i] = strcpy((*filenames)[i], dir);
-            (*filenames)[i] = strcat((*filenames)[i], "/");
-            (*filenames)[i] = strcat((*filenames)[i], ep->d_name);
+            (*filenames)[i] = get_relative_filename(dir, ep->d_name);
             i++;
         }
     }
+    closedir(dp);
     return n_files;
 }
 
+// Free space allocated to list of filenames.
 void freeFilenames(int n_files, char **filenames)
 {
     int i;
